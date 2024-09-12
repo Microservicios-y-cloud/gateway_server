6c10c1539fad67526091d79b91cc89f596942cdb
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-09-11 22:20:56 -0500

    pequeñas modificaciones

--- src/main/java/co/edu/javeriana/msc/turismo/gateway_server/config/KeycloakJwtAuthenticationConverter.java
+++ src/main/java/co/edu/javeriana/msc/turismo/gateway_server/config/KeycloakJwtAuthenticationConverter.java
@@ -18,30 +18,29 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import reactor.core.publisher.Mono;
 
 public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Mono<AbstractAuthenticationToken>> {
-  private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
 
-  @Override
-  public Mono<AbstractAuthenticationToken> convert(Jwt source) {
-      return Mono.just(source)
-              .flatMap(jwt -> {
-                  Collection<GrantedAuthority> authorities = Stream.concat(
-                          jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
-                          extractResourceRoles(jwt).stream())
-                          .collect(Collectors.toSet());
+    @Override
+    public Mono<AbstractAuthenticationToken> convert(Jwt source) {
+        return Mono.just(source)
+                .flatMap(jwt -> {
+                    Collection<GrantedAuthority> authorities = Stream.concat(
+                                    jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                                    extractResourceRoles(jwt).stream())
+                            .collect(Collectors.toSet());
+                    return Mono.just(new JwtAuthenticationToken(jwt, authorities));
+                });
+    }
 
-                  return Mono.just(new JwtAuthenticationToken(jwt, authorities));
-              });
-  }
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        var resourceAccess = new HashMap<>(jwt.getClaim("realm_access"));
 
-  private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-      var resourceAccess = new HashMap<>(jwt.getClaim("realm_access"));
+        @SuppressWarnings("unchecked")
+        var roles = (ArrayList<String>) resourceAccess.get("roles");
 
-      @SuppressWarnings("unchecked")
-      var roles = (ArrayList<String>) resourceAccess.get("roles");
-
-      Set<SimpleGrantedAuthority> authorities = roles.stream()
-              .map(role -> new SimpleGrantedAuthority(role))
-              .collect(Collectors.toSet());
-      return authorities;
-  }
+        Set<SimpleGrantedAuthority> authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors.toSet());
+        return authorities;
+    }
 }
